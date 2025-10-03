6c1e3c5edc01090ee18e1c084255247b9173b08b
Aurélio Venturelli <venturelli.dev@gmail.com>
2025-10-03 16:48:06 -0300

    refactor: Mudança de arquitetura para feature-first; feat: Adição do componente button seguindo padrões DRY e SRP.

---
+++ app/app/src/components/shared/button/Button.tsx
@@ -0,0 +1,24 @@
+import { ButtonProps } from "../../../types/shared/button.types";
+
+const BASE_BUTTON_CLASSES =
+	"text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700";
+
+const Button = ({
+	children,
+	className = "",
+	type = "button",
+	...rest
+}: ButtonProps) => {
+	const buttonClasses = `${BASE_BUTTON_CLASSES} ${className}`.trim();
+
+	return (
+		<button
+			type={type}
+			className={buttonClasses}
+			{...rest}>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
